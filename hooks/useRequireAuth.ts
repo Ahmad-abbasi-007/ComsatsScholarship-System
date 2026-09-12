3e6da6f06563e2a552907e9e58d875a3480b0de5
Ahmad-abbasi-007 <ar3359499@gmail.com>
2026-09-12 17:26:38 +0500

    Prepare project for Vercel deployment

--- hooks/useRequireAuth.ts
+++ hooks/useRequireAuth.ts
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '@/app/contexts/AuthContext';
 
 export function useRequireAuth() {
   const { user, loading } = useAuth();
